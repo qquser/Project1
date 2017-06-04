74835bc1778a92102876673c00171f10a9fd49a7
qquser <qquser@yandex.ru>
2017-06-04 12:49:03 +0400

    qw

---
+++ Project1.Client/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Client
+{
+    internal class City : IStandardEntity
+    {
+        private readonly string _name;
+        public City(string name)
+        {
+            _name = name;
+        }
+
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeInActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rename()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project1.Client/IStandardEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Client
+{
+    internal abstract class StandardEntity
+    {
+        public
+        public abstract void Add();
+        public abstract Rename();
+        public abstract MakeInActive();
+
+    }
+}
